46964552be02f2b14afee08f2f9d7a494977c85b
sereja32 <britanskiprod@gmail.com>
2017-06-28 13:35:35 +0300

    events

--- src/models/event.model.js
+++ src/models/event.model.js
@@ -7,7 +7,17 @@ module.exports = function (app) {
   const { Schema } = mongooseClient;
   const event = new Schema({
 
+    name: {type: String, required: true},
+    users: {type: Number, required: true, ref:'users'},
+    occuredAt: {type : Date ,required: true},
+    description:{type: String, required: true},
+    photos:[{type: Number, required: true , ref : 'photos'}],
+    location:[{type: Number , required :true}] ,
+    radius : {type: Number , required :true},
+    comments : {type: String , required: true, ref : 'comments'},
 
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
   });
 
   return mongooseClient.model('event', event);
--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 const users = require('./users/users.service.js');
 const authorities = require('./authorities/authorities.service.js');
+const event = require('./event/event.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(users);
   app.configure(authorities);
+  app.configure(event);
 };
